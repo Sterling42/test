bf40c73dc77b90b2b9f778147745ab259e522964
Test <deimudda@gmail.com>
2024-06-05 05:30:10 +0200

    t

--- pages/api/user.js
+++ pages/api/user.js
@@ -1,3 +1,5 @@
+export const config = { api: { externalResolver: true } };
+
 import dbConnect from '../../lib/mongodb';
 import User from '../../models/User';
 
@@ -8,37 +10,37 @@ export default async function handler(req, res) {
 
   switch (method) {
     case 'GET':
-  const { walletAddress } = req.query;
-  try {
-    const user = await User.findOne({ walletAddress });
-    if (user) {
-      res.status(200).json({ success: true, data: user });
-    } else {
-      // Return a successful response with a specific message
-      res.status(200).json({ success: false, message: 'User not found' });
-    }
-  } catch (error) {
-    res.status(400).json({ success: false, error: error.message });
-  }
-  break;
-  case 'POST':
-    try {
-      const existingUser = await User.findOne({ walletAddress: req.body.walletAddress });
-      if (existingUser) {
-        return res.status(400).json({ success: false, message: 'Wallet already registered' });
+      const { walletAddress } = req.query;
+      try {
+        const user = await User.findOne({ walletAddress });
+        if (user) {
+          res.status(200).json({ success: true, data: user });
+        } else {
+          // Return a successful response with a specific message
+          res.status(200).json({ success: false, message: 'User not found' });
+        }
+      } catch (error) {
+        res.status(400).json({ success: false, error: error.message });
       }
-  
-      // Create the new user
-      const user = new User({ walletAddress: req.body.walletAddress });
-  
-      // Save the user to the database
-      await user.save();
-  
-      res.status(201).json({ success: true, data: user });
-    } catch (error) {
-      res.status(400).json({ success: false, error: error.message });
-    }
-    break;
+      break;
+    case 'POST':
+      try {
+        const existingUser = await User.findOne({ walletAddress: req.body.walletAddress });
+        if (existingUser) {
+          return res.status(400).json({ success: false, message: 'Wallet already registered' });
+        }
+
+        // Create the new user
+        const user = new User({ walletAddress: req.body.walletAddress });
+
+        // Save the user to the database
+        await user.save();
+
+        res.status(201).json({ success: true, data: user });
+      } catch (error) {
+        res.status(400).json({ success: false, error: error.message });
+      }
+      break;
     case 'PUT':
       const { walletAddress: updateWalletAddress, stats, xGold, XP } = req.body;
       if (!updateWalletAddress) {
